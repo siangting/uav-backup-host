404abff47089cff20155fc7a240783e51f5afbdc
siangting <siangting200@gmail.com>
2026-06-13 13:48:28 +0800

    Add simple boot anime.

---
+++ boot_script_rpi5_anime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true
+do
+    clear
+    echo "      \\(^_^)/"
+    echo "         |"
+    echo "        / \\"
+    sleep 0.3
+
+    clear
+    echo "      <(^_^<)"
+    echo "         |"
+    echo "        / \\"
+    sleep 0.3
+
+    clear
+    echo "      (>^_^)>"
+    echo "         |"
+    echo "        / \\"
+    sleep 0.3
+done
